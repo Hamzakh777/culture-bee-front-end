f7c95cabff30d3dfc55d063a5aae68c57d50fbc1
Hamza <hamzabitup@gmail.com>
2019-11-30 22:58:44 +0100

    Added blog card for blog section

---
+++ components/LandingPage/Blog/BlogCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div 
+            class="h-105 bg-center bg-cover"
+            style="background-image: url('/images/blog/image-1.jpg')"
+        ></div>
+        <div class="px-7-1/2 pt-9">
+            <span class="inline-block sub-title mb-5">blog</span>
+            <h4 class="mb-5 text-4-3/4 font-bold text-gray-800">Lorem ipsum dolor sit amet, consectetur </h4>
+            <span class="text-xs font-bold text-gray-800 tracking-widest uppercase">featured</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BlogCard'
+    }
+</script>
