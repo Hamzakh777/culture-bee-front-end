70752e7fae754ff1b2c3eeba94130d9482704f34
Hamza <hamzabitup@gmail.com>
2019-11-30 10:21:23 +0100

    Added css components, removed index boilerplat

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,17 @@
 	<div>
         <the-nav-bar />
 		<nuxt />
+        <the-footer />
 	</div>
 </template>
 <script>
 import TheNavBar from '@/components/NavBar/TheNavBar';
+import TheFooter from '@/components/Footer/TheFooter';
 
 export default {
     components: {
-        TheNavBar
+        TheNavBar,
+        TheFooter
     }
 };
 </script>
