c20df68f3f45717c6b8627d2246b62ba5584b3bc
Hamza <hamzabitup@gmail.com>
2019-12-08 16:15:02 +0100

    Add responsivness to hero section

--- components/LandingPage/Hero/TheLandingPageHero.vue
+++ components/LandingPage/Hero/TheLandingPageHero.vue
@@ -2,15 +2,15 @@
     <!-- the height should be dynamically calculated using the window height - nav height -->
 	<div 
         id="hero"
-        class="flex flex-col relative w-full" 
+        class="flex flex-col relative md:w-full" 
         style="height: 84vh"
     >
 		<!-- the slider wrapper -->
-		<div class="container mx-auto relative flex-grow">
-            <div v-swiper:mySwiper="swiperOption">
+		<div class="container flex flex-col md:block mx-auto relative md:flex-grow">
+            <div v-swiper:mySwiper="swiperOption" class="h-105  md:h-full">
                 <div class="swiper-wrapper">
                     <div
-                        class="swiper-slide bg-cover bg-center"
+                        class="swiper-slide bg-cover bg-no-repeat bg-center"
                         style="background-image: url('/images/blog/hero-1.jpg')"
                         v-for="i in 5"
                         :key="i"
@@ -18,7 +18,7 @@
                         <div
                             class="container relative mx-auto p-10 h-full"
                         >
-                            <span class="section-title section-title-light absolute left-0 top-0 mt-10 ml-10">hot topic</span>
+                            <span class="section-title section-title--light absolute left-0 top-0 mt-4 ml-4 md:mt-10 md:ml-10">hot topic</span>
                         </div>
                     </div>
                 </div>
@@ -26,11 +26,11 @@
                 <div class="button-next arrow right-0 -mr-24"  style="transform: rotate(45deg)"></div>
             </div>
 			<!-- the white card -->
-			<div class="container absolute left-1/2 bottom-0 z-1 h-1 -transform-x-50">
-				<div class="absolute bottom-0 left-0 z-10 ml-10 px-16 py-12-1/2 w-140 bg-white">
+			<div class="container relative md:absolute md:left-0 md:right-0 md:bottom-0 z-1 md:h-1">
+				<div class="md:absolute md:bottom-0 md:left-0 z-10 md:ml-10 px-4 md:px-16 py-12-1/2 w-full md:w-140 bg-white">
 					<span class="sub-title mb-6">Startups</span>
 					<h3
-						class="text-8 text-gray-900 font-poppins font-bold"
+						class="text-5-3/4 md:text-8 text-gray-900 font-poppins font-bold"
 					>A four-day week is being adopted by this startup</h3>
 				</div>
 			</div>
@@ -90,7 +90,7 @@ export default {
 
 <style scoped>
 .swiper-container {
-    @apply h-full overflow-visible;
+    @apply overflow-visible;
 }
 .swiper-wrapper .swiper-slide {
 	@apply opacity-75;
