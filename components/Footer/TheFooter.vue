45f4bb93f16ea0588b4b1de38d8aa319ca5479d1
Hamza <hamzabitup@gmail.com>
2019-12-09 17:16:31 +0100

    Update footer

--- components/Footer/TheFooter.vue
+++ components/Footer/TheFooter.vue
@@ -1,13 +1,20 @@
 <template>
     <div>
-        
-        
+        <footer-top />
+        <footer-bottom />
     </div>
 </template>
 
 <script>
+    import FooterTop from './FooterTop';
+    import FooterBottom from './FooterBottom';
 
     export default {
-        name: 'TheFooter'
+        name: 'TheFooter',
+
+        components: {
+            FooterTop,
+            FooterBottom
+        }
     }
 </script>
