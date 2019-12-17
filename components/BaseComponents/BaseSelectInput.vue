2dc224cbb619156e55a8c7e0735d1b1e14c79559
Hamza <hamzabitup@gmail.com>
2019-12-17 20:56:39 +0100

    Fix: use valid tailwindcss classes and styles

--- components/BaseComponents/BaseSelectInput.vue
+++ components/BaseComponents/BaseSelectInput.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="relative">
-        <div class="absolute top-1/2 left-0 -transform-y-50 w-10/12 ml-3 text-xs font-medium" v-if="!value">
+        <div class="absolute top-1/2 left-0 -transform-y-50 w-10/12 ml-6 text-sm text-gray-900 font-semibold" v-if="!value">
             {{placeholder}}
         </div>
         <select 
-            class="relative h-10 w-full pl-3 pr-7 rounded-none bg-white text-xs shadow-blue focus:outline-none"
+            class="relative h-12 w-full pl-3 pr-7 border-gray-700 border-2 rounded-none bg-white text-sm text-gray-900 font-semibold focus:outline-none"
             :value="value"
             @input="$emit('input', $event.target.value)"
         >
@@ -49,11 +49,11 @@ select {
     -webkit-appearance: none;
     -moz-appearance: none;
     appearance: none;
-    background: url(/assets/icons/arrow-down.svg) 88% no-repeat 
+    background: url(/icons/arrow-down.svg) 94% no-repeat 
 }
 select::before { 
     content: '';
-    @apply absolute right-0 h-3 w-3 border-t-2 border-r-2 border-blue-800;
+    @apply absolute right-0 h-3 w-3 border-t-2 border-r-2 border-blue;
 }
 /* CAUTION: IE hackery ahead */
 select::-ms-expand { 
