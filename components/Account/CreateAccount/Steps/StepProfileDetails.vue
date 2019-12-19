8b5fed0d22b6c38d18c3a75d4c0955244fc2ec19
Hamza <hamzabitup@gmail.com>
2019-12-19 22:33:36 +0100

    Add: show selected image to upload

--- components/Account/CreateAccount/Steps/StepProfileDetails.vue
+++ components/Account/CreateAccount/Steps/StepProfileDetails.vue
@@ -1,18 +1,25 @@
 <template>
     <div class="py-6">
         <div class="flex items-end mb-6">
-            <div class="flex justify-center items-center p-3 w-20 h-20 border-2 border-gray-700 bg-yellow">
-                <img 
-                    src="/images/global/icons/smile-face.png"
-                >
+            <div 
+                class="flex justify-center items-center p-3 w-20 h-20 border-2 border-gray-700 bg-yellow bg-center bg-cover bg-no-repeat"
+                :style="'background-image: url(' + (files.length ? files[0].url : '/images/global/icons/smile-face.png') + '); background-size:' + (files.length ? 'cover' : '90% 90%') "
+            >
+                <!-- <img 
+                    :src="files.length ? files[0].url : '/images/global/icons/smile-face.png'"
+                > -->
             </div>
-            <label class="relative ml-4 font-medium text-gray-800 cursor-pointer underline">
-                <input
-                    class="absolute h-0 w-0 opacity-0" 
-                    type="file"
-                >
+            <file-upload
+                class="relative ml-4 font-medium text-gray-800 cursor-pointer underline"
+                extensions="gif,jpg,jpeg,png,webp"
+                accept="image/png,image/gif,image/jpeg,image/webp"
+                name="avatar"
+                v-model="files"
+                @input-file="inputFile"
+                ref="upload"
+            >
                 Upload your profile photo
-            </label>
+            </file-upload>
         </div>
         <input 
             class="input-text w-full mb-4"
@@ -40,15 +47,18 @@
 <script>
     import {required, minLength} from 'vuelidate/lib/validators';
     import { mapState, mapMutations } from 'vuex';
+    import FileUpload from 'vue-upload-component';
     import StepsNav from './StepsNav';
     import BaseInputErrorMessage from '~/components/BaseComponents/BaseInputErrorMessage';
+
 
     export default {
         name: 'StepProfileDetails',
 
         components: {
             StepsNav,
-            BaseInputErrorMessage
+            BaseInputErrorMessage,
+            FileUpload
         },
 
         computed: {
@@ -62,8 +72,25 @@
             }
         },
 
+        data() {
+            return {
+                files: []
+            }
+        },
+
         methods: {
             ...mapMutations('account/create', ['mutate', 'nextStep', 'previousStep']),
+
+            inputFile(newFile, oldFile) {
+                if (newFile && (!oldFile || newFile.file !== oldFile.file)) {
+                    newFile.url = ''
+                    const URL = window.URL || window.webkitURL;
+                    if (URL && URL.createObjectURL) {
+                        newFile.url = URL.createObjectURL(newFile.file);
+                    }
+                    this.files.push(newFile);
+                }
+            },
 
             setName(name) {
                 console.log(name);
