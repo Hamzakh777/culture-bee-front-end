9a6e5be83ec157b65e74f304045cfe50be6c666d
Hamza <hamzabitup@gmail.com>
2019-12-24 16:14:28 +0100

    Improvement: making it more resuable

---
+++ components/Profile/Employer/BenefitCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex flex-col justify-between h-118 w-31-% bg-gray-100">
+        <div class="mt-6 mr-6">
+            <base-close-button 
+                @click="$emit('remove-the-benefit')"
+            />
+        </div>
+        <div>
+            <div class="">
+                Breakfat
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import BaseCloseButton from '~/components/BaseComponents/BaseCloseButton';
+
+    export default {
+        name: 'BenefitCard',
+        
+        components: {
+            BaseCloseButton
+        }
+    }
+</script>
