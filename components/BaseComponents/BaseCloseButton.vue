5677c248833322da8a42fec551aa323edd40205e
Hamza <hamzabitup@gmail.com>
2019-12-24 16:00:00 +0100

    Add: base close button component

--- components/BaseComponents/BaseCloseButton.vue
+++ components/BaseComponents/BaseCloseButton.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-
+    <div
+        @click="$emit('click')"
+        class="absolute right-0 top-1/2 h-9 w-9 bg-yellow -transform-y-50 mr-10"
+    >
+        <div class="cross"></div>
     </div>
 </template>
 
@@ -9,3 +12,22 @@
         name: 'BaseCloseButton'
     }
 </script>
+
+<style scoped>
+.cross::after,
+.cross::before {
+	content: '';
+	transform: translate(-50%, -50%) rotate(45deg);
+	@apply absolute top-1/2 left-1/2 bg-gray-800;
+}
+
+.cross::after {
+	width: 3px;
+	@apply h-5;
+}
+
+.cross::before {
+	height: 3px;
+	@apply w-5;
+}  
+</style>
