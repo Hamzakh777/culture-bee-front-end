ce0f180c52d6ac913f2177ef9901eeb83fad4aea
Hamza <hamzabitup@gmail.com>
2019-12-26 12:06:36 +0100

    Add: hand picked update section

---
+++ components/Feed/FeedCard.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class="shadow-md">
+		<div
+			:style="`background-image: url(${update.coverImgUrl})`"
+			class="relative h-138 bg-gray-200 bg-center bg-cover"
+		>
+			<!-- like icon -->
+			<base-like-icon class="top-0 right-0 md:mt-8" />
+
+			<!-- company logo -->
+			<div
+				class="hidden md:flex justify-center items-center absolute top-0 left-0 h-32 w-32 mt-8 ml-8 p-2 bg-white"
+			>
+				<img :src="update.logoUrl" class="w-full" />
+			</div>
+		</div>
+		<div class="px-7-1/2 py-9 bg-white">
+			<span class="inline-block sub-title mb-5">update</span>
+			<h4 class="mb-5 text-lg md:text-8 font-bold text-gray-800">
+				{{ update.description }}
+			</h4>
+			<div
+				class="text-xs font-bold text-gray-800 tracking-widest uppercase"
+			>
+				<span
+					v-for="(tag, index) in update.tags"
+					:key="index"
+					class="inline-block mb-2 mr-4"
+				>
+					{{ tag }}
+				</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import BaseLikeIcon from '~/components/BaseComponents/BaseLikeIcon';
+
+export default {
+	name: 'FeedCard',
+
+	components: {
+		BaseLikeIcon
+	},
+
+	props: {
+		update: {
+			type: Object,
+			required: true
+		}
+	}
+};
+</script>
--- components/Feed/FeedNotLogedIn.vue
+++ components/Feed/FeedNotLogedIn.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="mt-16">
+    <div>
         <!-- companies section -->
-        <div>
+        <div class="mt-16">
             <div class="base-title">
                 GET CULTURE INSPO FROM OTHER COMPANIES
             </div>
@@ -21,17 +21,39 @@
                 </nuxt-link>
             </div>
         </div>
+        <!-- handpicker update by culture bee -->
+        <div class="mt-16">
+            <div class="base-title mb-10">
+                HAND-PICKED BY CULTUREBEE
+            </div>
+            <div>
+                <feed-card :update="handPickedUpdate" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import CompanieCard from '../Companies/CompanieCard';
+    import FeedCard from './FeedCard';
 
     export default {
         name: 'FeedNotLogedIn',
 
         components: {
-            CompanieCard
-        }
+            CompanieCard,
+            FeedCard
+        },
+
+        data() {
+            return {
+                handPickedUpdate: {
+                    logoUrl: '/logos/brave-gentle-man.png',
+                    coverImgUrl: '/images/company-updates/update-1.jpg',
+                    tags: ['volunteer', 'csr', 'pride'],
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+                }
+            }
+        },
     }
 </script>
