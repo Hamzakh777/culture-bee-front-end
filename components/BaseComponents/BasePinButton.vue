15c850612778a3abfa8bda376f114b39a0dba00b
Hamza <hamzabitup@gmail.com>
2020-01-04 19:31:30 +0100

    Add: base pin button

---
+++ components/BaseComponents/BasePinButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <div 
+        class="flex items-center justify-center h-12-1/2 w-12-1/2 cursor-pointer"
+        :class="{'bg-black text-white': !pinned, 'bg-yellow text-gray-800': pinned}"
+        @click="$emit('update:pinned', !pinned)"
+    >
+        <base-app-icon 
+            name="pin"
+            :class-list="['h-7', 'fill-current']"
+        />
+    </div>
+</template>
+
+<script>
+    import BaseAppIcon from './BaseAppIcon';
+
+    export default {
+        name: 'BasePinButton',
+
+        components: {
+            BaseAppIcon
+        },
+
+        props: {
+            pinned: {
+                type: Boolean,
+                required: true
+            }
+        }
+    }
+</script>
