7a7bfea9b8260ee97807616412099eb0f0438121
Hamza <hamzabitup@gmail.com>
2020-01-04 11:46:46 +0100

    Add: ability to select icons

--- components/Profile/Employer/EditModals/EditValuesModal.vue
+++ components/Profile/Employer/EditModals/EditValuesModal.vue
@@ -2,21 +2,73 @@
 	<base-modal :is-active="isActive" @close="toggle">
 		<template #title>Add values</template>
 		<template #content>
-			<div class="base-title mb-6">what does your company believe in?</div>
+			<div class="base-title mb-6">
+				what does your company believe in?
+			</div>
 			<div class="mb-8">
 				<div
-					v-for="(value, key, index) in values"
+					v-for="(value, key, index) in clonedValues"
 					:key="index"
 					class="flex flex-row items-center w-full mb-3 p-2 border-2 border-gray-800"
 				>
 					<!-- icon -->
-					<div class="w-10"></div>
+					<div 
+						class="relative w-10 ml-4"
+					>
+						<!-- plus -->
+						<div 
+							v-if="value.icon == false"
+							class="plus"
+							@click="setActiveIconSelector(index)"
+						>
+						</div>
+						<div
+							v-else
+							class="absolute top-1/2 left-1/2 transform-center flex justify-center items-center h-12 w-12 rounded-full border-2 border-gray-800 cursor-pointer"
+							@click="setActiveIconSelector(index)"
+						>
+							<font-awesome-icon
+								:icon="value.icon"
+								class="h-5 text-13"
+							/>
+						</div>
+
+						<!-- list to choose an icon -->
+						<div 
+							class="absolute top-0 left-0 z-10 mt-8"
+							v-if="activeIconSelector === index"
+							v-on-clickaway="setActiveIconSelector"
+						>
+							<v-select
+								:options="fontawesomeIcons"
+								:clearable="false"
+								class="bg-white w-40 shadow-xl"
+								v-model="value.icon"
+							>
+								<!-- selected option -->
+								<template
+									v-slot:selected-option="slotProps"
+								>
+									<font-awesome-icon
+										:icon="slotProps.label"
+										class="h-5 text-13"
+									/>
+								</template>
+								<!-- dropdown -->
+								<template v-slot:option="slotProps">
+									<font-awesome-icon
+										:icon="slotProps.label"
+										class="h-5 text-13"
+									/>
+								</template>
+							</v-select>
+						</div>
+					</div>
 					<input
-						:value="value.title"
-						@input="setTitle(key, $event.target.value)"
 						class="flex-grow input-text border-none"
 						type="text"
 						placeholder="Add value"
+						v-model="value.title"
 					/>
 					<div class="relative w-10 h-4">
 						<base-close-button
@@ -28,31 +80,58 @@
 				</div>
 			</div>
 			<div class="flex justify-end">
-				<base-ajax-button :is-loading="isLoading" @click="submit">Add to profile</base-ajax-button>
+				<base-ajax-button :is-loading="isLoading" @click="submit"
+					>Add to profile</base-ajax-button
+				>
 			</div>
 		</template>
 	</base-modal>
 </template>
 
 <script>
+import { mixin as clickaway } from 'vue-clickaway';
 import { mapState, mapMutations } from 'vuex';
+import vSelect from 'vue-select';
 import BaseModal from '~/components/BaseComponents/BaseModal';
 import BaseAjaxButton from '~/components/BaseComponents/BaseAjaxButton';
 import BaseCloseButton from '~/components/BaseComponents/BaseCloseButton';
+import fontawesomeIcons from '~/assets/data/fontawesomeIcons';
 
 export default {
 	name: 'EditValuesModal',
 
+	mixins: [clickaway],
+
 	components: {
 		BaseModal,
 		BaseAjaxButton,
-		BaseCloseButton
+		BaseCloseButton,
+		vSelect
+	},
+
+	watch: {
+		clonedValues: {
+			handler(newVal) {
+				const payload = {
+					property: 'values',
+					with: newVal
+				};
+
+				console.log(payload);
+				// this.mutate(payload);
+			},
+
+			deep: true
+		}
 	},
 
 	data() {
 		return {
+			fontawesomeIcons,
 			isActive: false,
-			isLoading: false
+			isLoading: false,
+			clonedValues: null,
+			activeIconSelector: null 
 		};
 	},
 
@@ -64,14 +143,12 @@ export default {
 		this.$bus.$on('open-employer-values-modal', () => {
 			this.toggle();
 		});
+		// close the data in the store
+		this.clonedValues = JSON.parse(JSON.stringify(this.values));
 	},
 
 	methods: {
-		...mapMutations('employer', [
-			'setValueTitle',
-			'resetValue',
-			'nextStep'
-		]),
+		...mapMutations('employer', ['nextStep', 'mutate']),
 
 		submit() {
 			this.toggle();
@@ -82,14 +159,36 @@ export default {
 			this.isActive = !this.isActive;
 		},
 
-		setTitle(key, title) {
-			const payload = {
-				key,
-				title
-			};
+		resetValue(key) {
+			this.clonedValues[key].title = null;
+			this.clonedValues[key].icon = null;
+		},
 
-			this.setValueTitle(payload);
+		setActiveIconSelector(index = null) {
+			if(this.activeIconSelector === null) {
+				this.activeIconSelector = index;
+			} else {
+				this.activeIconSelector = null;
+			}
 		}
 	}
 };
 </script>
+<style scoped>
+.plus {
+	@apply relative h-4 w-4 mx-auto cursor-pointer;
+}
+.plus::before,
+.plus::after {
+	content: '';
+	@apply absolute top-1/2 left-1/2 transform-center rounded-full bg-gray-800;
+}
+.plus::before {
+	height: 3px;
+	@apply w-full;
+}
+.plus::after {
+	width: 3px;
+	@apply h-full;
+}
+</style>
--- store/employer/mutations.js
+++ store/employer/mutations.js
@@ -2,13 +2,6 @@ const mutations = {
     mutate(state, payload) {
         state[payload.property] = payload.with;
     },
-    setValueTitle(state, payload) {
-        state.values[payload.key].title = payload.title;
-    },
-    resetValue(state, key) {
-        state.values[key].title = null;
-        state.values[key].icon = null;
-    },
     nextStep(state) {
         state.currentProfileCreationStep += 1;
     },
