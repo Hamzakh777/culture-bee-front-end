15d99b1397cd30fe39fb95bb3c16c18b0d5207ef
Hamza <hamzabitup@gmail.com>
2020-01-05 11:06:17 +0100

    Improvement: seperating employer pages (edit, view)

---
+++ pages/employer/_id/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<employer-hero />
+		<div class="container mx-auto py-10">
+			<employer-account-progress />
+		</div>
+		<employer-profile-sections />
+		<employer-edit-profile-modals /> 
+	</div>
+</template>
+
+<script>
+import EmployerHero from '~/components/Profile/Employer/EmployerHero';
+import EmployerAccountProgress from '~/components/Profile/Employer/EmployerAccountProgress';
+import EmployerEditProfileModals from '~/components/Profile/Employer/EmployerEditProfileModals';
+import EmployerProfileSections from '~/components/Profile/Employer/EmployerProfileSections';
+
+export default {
+	components: {
+		EmployerHero,
+		EmployerAccountProgress,
+		EmployerEditProfileModals,
+		EmployerProfileSections
+	},
+
+	mounted() {
+		if(process.browser) {
+			localStorage.setItem('access_token', 'lkashdflkjhasdklfjklasdjfklasjdlfasdf');
+		}
+	}
+};
+</script>
