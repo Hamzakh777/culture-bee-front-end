bc3625d52760a7848769d894af8b19d0ef8d28a5
Hamza <hamzabitup@gmail.com>
2020-01-06 09:32:51 +0100

    Fix: purgeCss removing fontawesome css

--- plugins/vue-fontawesome.js
+++ plugins/vue-fontawesome.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
+/* purgecss start ignore */
 import { library, config } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+/* purgecss end ignore */
 
 config.autoAddCss = false;
 
