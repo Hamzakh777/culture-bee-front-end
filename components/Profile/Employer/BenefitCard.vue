0a0ad1874dc0a889ad59d4e7199656d12a774e74
Hamza <hamzabitup@gmail.com>
2020-01-10 10:31:54 +0100

    Improvement: prettier

--- components/Profile/Employer/BenefitCard.vue
+++ components/Profile/Employer/BenefitCard.vue
@@ -1,63 +1,65 @@
 <template>
-    <div 
-        class="relative h-118 mb-14 w-31-% bg-gray-200 bg-center bg-cover bg-no-repeat"
-        :style="benefit.imgUrl !== null ?  `background-image: url(${benefit.imgUrl})` : ''"
-    >
-        <div class="flex flex-col justify-between relative z-1 h-full w-full">
-            <div 
-                v-if="isEditPage"
-                class="flex justify-end mt-6 mr-6"
-            >
-                <!-- emit the id too -->
-                <base-edit-pen 
-                    class="h-9 w-9 mr-4 bg-gray-800 text-white hover:text-yellow text-yello"
-                    :pen-class-list="['h-5', 'top-1/2', 'left-1/2', 'transform-center']"
-                    @click="$emit('edit-benefit')"
-                />
-                <base-close-button 
-                    @click="$emit('remove-the-benefit')"
-                />
-            </div>
-            <div v-else></div>
-            <div class="px-8 py-10 text-white">
-                <!-- title -->
-                <div class="font-D-Din text-5xl font-bold uppercase">
-                    {{ benefit.title }}
-                </div>
-                <!-- subtitle -->
-                <div class="text-5-1/2 font-bold">
-                    {{ benefit.subtitle }}
-                </div>
-            </div>
-        </div>
-        <div class="absolute inset-0 bg-transparent-to-black opacity-75">
-
-        </div>
-    </div>
+	<div
+		:style="
+			benefit.imgUrl !== null
+				? `background-image: url(${benefit.imgUrl})`
+				: ''
+		"
+		class="relative h-118 mb-14 w-31-% bg-gray-200 bg-center bg-cover bg-no-repeat"
+	>
+		<div class="flex flex-col justify-between relative z-1 h-full w-full">
+			<div v-if="isEditPage" class="flex justify-end mt-6 mr-6">
+				<!-- emit the id too -->
+				<base-edit-pen
+					:pen-class-list="[
+						'h-5',
+						'top-1/2',
+						'left-1/2',
+						'transform-center'
+					]"
+					class="h-9 w-9 mr-4 bg-gray-800 text-white hover:text-yellow text-yello"
+					@click="$emit('edit-benefit')"
+				/>
+				<base-close-button @click="$emit('remove-the-benefit')" />
+			</div>
+			<div v-else></div>
+			<div class="px-8 py-10 text-white">
+				<!-- title -->
+				<div class="font-D-Din text-5xl font-bold uppercase">
+					{{ benefit.title }}
+				</div>
+				<!-- subtitle -->
+				<div class="text-5-1/2 font-bold">
+					{{ benefit.subtitle }}
+				</div>
+			</div>
+		</div>
+		<div class="absolute inset-0 bg-transparent-to-black opacity-75"></div>
+	</div>
 </template>
 
 <script>
-    import BaseCloseButton from '~/components/BaseComponents/BaseCloseButton';
-    import BaseEditPen from '~/components/BaseComponents/BaseEditPen';
+import BaseCloseButton from '~/components/BaseComponents/BaseCloseButton';
+import BaseEditPen from '~/components/BaseComponents/BaseEditPen';
 
-    export default {
-        name: 'BenefitCard',
+export default {
+	name: 'BenefitCard',
 
-        props: {
-            isEditPage: {
-                type: Boolean,
-                required: false,
-                default: false
-            },
-            benefit: {
-                type: Object,
-                required: true
-            }
-        },
-        
-        components: {
-            BaseCloseButton,
-            BaseEditPen
-        }
-    }
+	props: {
+		isEditPage: {
+			type: Boolean,
+			required: false,
+			default: false
+		},
+		benefit: {
+			type: Object,
+			required: true
+		}
+	},
+
+	components: {
+		BaseCloseButton,
+		BaseEditPen
+	}
+};
 </script>
