383db85d6d9881ad4badd9477d2c17e747d36d52
Hamza <hamzabitup@gmail.com>
2020-01-17 17:31:34 +0100

    Imporovment: generate the empty benefits dynamically

--- components/Profile/Employer/EditModals/AddBenefitsModal.vue
+++ components/Profile/Employer/EditModals/AddBenefitsModal.vue
@@ -135,59 +135,29 @@ export default {
 			isActive: false,
 			isLoading: false,
 			currentActiveBenefit: 1, // the benefit being edited
-			benefits: [
-				{
-					id: 1,
-					title: 'Breakfast',
-					subtitle: 'A mix of cereals, pastry and fruits available',
-					imgFile: null,
-					imgUrl: ''
-				},
-				{
-					id: 2,
-					title: 'yoga classes',
-					subtitle: 'With our lovely resident wellness coach Hillary',
-					imgFile: null,
-					imgUrl: ''
-				},
-				{
-					id: 3,
-					title: 'Flexible',
-					subtitle: 'Flexible working to fit around your lifestyle',
-					imgFile: null,
-					imgUrl: ''
-				},
-				{
-					id: 4,
-					title: 'socials',
-					subtitle: 'Monthly offsite socials to encourage teambuilding',
-					imgFile: null,
-					imgUrl: ''
-				},
-				{
-					id: 5,
-					title: 'GROWTH',
-					subtitle: 'We send you on self-development courses',
-					imgFile: null,
-					imgUrl: ''
-				},
-				{
-					id: 6,
-					title: 'BRUNCH',
-					subtitle: 'Monthly bonding bunch with the whole team',
-					imgFile: null,
-					imgUrl: ''
-				}
-			]
+			benefits: []
 		};
 	},
+
+
 
 	created() {
 		this.$bus.$on('open-employer-add-benefits-modal', () => {
 			this.reset();
 			this.toggle();
-		});
 
+			while (this.benefits.length !== 6) {
+				const emptyValue = {
+					id: 1,
+					title: null,
+					subtitle: null,
+					imgFile: null,
+					imgUrl: ''
+				};
+				
+				this.benefits.push(emptyValue);
+			}
+		});
 	},
 
 	validations: {
