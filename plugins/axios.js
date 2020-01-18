38c217548485537ae0a45a31b23064661d0eed96
Hamza <hamzabitup@gmail.com>
2020-01-18 11:43:03 +0100

    Add: redirect to 404

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,11 +1,14 @@
-export default function({ $axios, redirect }) {
+export default function ({ $axios, redirect }) {
 	$axios.onError(error => {
 		const code = parseInt(error.response && error.response.status);
 		if (code === 401) {
 			if (localStorage.getItem('access_token') !== null) {
 				localStorage.removeItem('acces_token');
 			}
-			redirect('/login');
+			return redirect('/login');
+		}
+		if (code === 404) {
+			return redirect('/404')
 		}
 	});
 }
