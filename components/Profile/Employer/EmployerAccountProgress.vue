6df9ae095e0da87e6767a660ae4a9e093b9aa5cc
Hamza <hamzabitup@gmail.com>
2020-01-21 16:39:11 +0100

    Improvement: make the account progress responsive

--- components/Profile/Employer/EmployerAccountProgress.vue
+++ components/Profile/Employer/EmployerAccountProgress.vue
@@ -1,43 +1,46 @@
 <template>
-	<div class="p-16 bg-gray-100">
-		<!-- step title -->
-		<div
-			class="text-13 font-bold font-D-Din text-black uppercase tracking-tight"
-		>{{ steps[currentProfileCreationStep].section }}</div>
-		<!-- progress bar -->
-		<div class="flex justify-between mt-5 mb-9">
+	<div class="relative px-6 py-10 md:p-16 bg-gray-100">
+		<div class="relative z-1">
+			<!-- step title -->
 			<div
-				v-for="step in totalSteps"
-				:key="step"
-				:class="{ 'bg-yellow': step === currentProfileCreationStep }"
-				class="h-4 bg-gray-200"
-				style="width: 16.4%"
-			></div>
-		</div>
-		<!-- step details -->
-		<div class="flex px-8 py-10 border-2 border-gray-900 bg-white">
-			<div>
-				<img :src="steps[currentProfileCreationStep].iconUrl" class="w-14" />
-			</div>
-			<div class="pl-8">
-				<!-- step title -->
-				<div class="mb-3 font-bold text-black text-5-1/2">{{ steps[currentProfileCreationStep].title }}</div>
-				<!-- step description -->
+				class="text-8 md:text-13 font-bold font-D-Din text-black uppercase tracking-tight"
+			>{{ steps[currentProfileCreationStep].section }}</div>
+			<!-- progress bar -->
+			<div class="flex justify-between mt-4 md:mt-5 mb-4 md:mb-9">
 				<div
-					class="font-normal text-black text-base"
-				>{{ steps[currentProfileCreationStep].description }}</div>
+					v-for="step in totalSteps"
+					:key="step"
+					:class="{ 'bg-yellow': step === currentProfileCreationStep }"
+					class="h-3 md:h-4 bg-gray-200"
+					style="width: 16%"
+				></div>
+			</div>
+			<!-- step details -->
+			<div class="flex flex-col items-center md:items-start md:flex-row px-8 py-10 border-2 border-gray-900 bg-white">
+				<div>
+					<img :src="steps[currentProfileCreationStep].iconUrl" class="w-14 mb-6 md:mb-0" />
+				</div>
+				<div class="md:pl-8">
+					<!-- step title -->
+					<div class="mb-3 font-bold text-black text-5-1/2 text-center md:text-left">{{ steps[currentProfileCreationStep].title }}</div>
+					<!-- step description -->
+					<div
+						class="font-normal text-black text-base text-center md:text-left"
+					>{{ steps[currentProfileCreationStep].description }}</div>
+				</div>
+			</div>
+			<div class="flex justify-between lg:justify-end items-center mt-10">
+				<button
+					@click="prevStep"
+					v-if="currentProfileCreationStep !== 1"
+					class="secondary-btn mr-16"
+				>
+					back
+				</button>
+				<button @click="next" class="primary-btn">{{ steps[currentProfileCreationStep].nextButtonText }}</button>
 			</div>
 		</div>
-		<div class="flex justify-end items-center mt-10">
-			<button
-				@click="prevStep"
-				v-if="currentProfileCreationStep !== 1"
-				class="secondary-btn mr-16"
-			>
-				back
-			</button>
-			<button @click="next" class="primary-btn">{{ steps[currentProfileCreationStep].nextButtonText }}</button>
-		</div>
+		<div class="md:hidden absolute top-0 bottom-0 left-1/2 w-screen -transform-x-50 bg-gray-100"></div>
 	</div>
 </template>
 
