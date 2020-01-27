02099a41f05d0e64b31887839067ba0b88a1232a
Hamza <hamzabitup@gmail.com>
2020-01-27 22:33:56 +0100

    Fix: getProfileDetails should return a promise

--- store/employer/actions.js
+++ store/employer/actions.js
@@ -1,27 +1,30 @@
 const actions = {
-    async getProfileDetails({ commit }, id) {
-        try {
-            const response = await this.$axios.get(`api/employer/${id}`);
-
-            commit('setProfileDetails', response.data.data);
-        } catch (error) {
-            if (process.browser) {
-                window.alert('An error occured trying to retrieve the user data');
-            }
-        }
-    },
-    updateAccountDetails({ commit }, data) {
-        return new Promise((resolve, reject) => {
-            this.$axios.post('/api/profile', data)
-                .then((response) => {
-                    commit('setProfileDetails', response.data.user);
-                    resolve(response);
-                })
-                .catch(err => {
-                    reject(err);
-                })
-        })
-    },
-}
+	getProfileDetails({ commit }, id) {
+		return new Promise((resolve, reject) => {
+			this.$axios
+				.get(`api/employer/${id}`)
+				.then(response => {
+					commit('setProfileDetails', response.data.data);
+					resolve(response);
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	},
+	updateAccountDetails({ commit }, data) {
+		return new Promise((resolve, reject) => {
+			this.$axios
+				.post('/api/profile', data)
+				.then(response => {
+					commit('setProfileDetails', response.data.user);
+					resolve(response);
+				})
+				.catch(err => {
+					reject(err);
+				});
+		});
+	}
+};
 
 export default actions;
