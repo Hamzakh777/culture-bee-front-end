6d5fc0907307816c41b164b96a22e9e537dbb3de
Hamza <hamzabitup@gmail.com>
2020-01-27 18:57:55 +0100

    Add: job advert skill section

---
+++ components/Jobs/Sections/SkillsSection.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="py-6 bg-gallery">
+        <div class="container md:flex mx-auto">
+            <div class="section-title">
+                skills
+            </div>
+            <div class="md:flex items-start flex-wrap mt-8 md:mt-0 md:pl-8 lg:pl-16 md:pt-3">
+                <div
+                    v-for="(skill, index) in skills"
+                    :key="index"
+                    class="mb-4 last:mb-0 md:mr-4 py-3 px-4 md:px-6 bg-white text-center text-xs text-gray-900 tracking-widest font-bold"
+                >
+                    {{ skill }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { mapState } from 'vuex';
+
+    export default {
+        name: 'SkillsSection',
+
+        computed: {
+            ...mapState('job', ['skills'])
+        },
+    }
+</script>
---
+++ pages/jobs/_id/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <skills-section />
+    </div>
+</template>
+
+<script>
+    import SkillsSection from '~/components/Jobs/Sections/SkillsSection';
+    
+    export default {
+
+        components: {
+            SkillsSection
+        }
+    }
+</script>
