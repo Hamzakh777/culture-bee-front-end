faafe517b9d5e94a46e8e5a71203e70157b44914
Hamza <hamzabitup@gmail.com>
2020-01-27 15:22:04 +0100

    Fix: going back to previous step doesn't remove sections

--- components/Profile/Employer/EmployerAccountProgress.vue
+++ components/Profile/Employer/EmployerAccountProgress.vue
@@ -4,13 +4,13 @@
 			<!-- step title -->
 			<div
 				class="text-8 md:text-13 font-bold font-D-Din text-black uppercase tracking-tight"
-			>{{ steps[currentProfileCreationStep].section }}</div>
+			>{{ steps[currentStep].section }}</div>
 			<!-- progress bar -->
 			<div class="flex justify-between mt-4 md:mt-5 mb-4 md:mb-9">
 				<div
 					v-for="step in totalSteps"
 					:key="step"
-					:class="{ 'bg-yellow': step === currentProfileCreationStep }"
+					:class="{ 'bg-yellow': step === currentStep }"
 					class="h-3 md:h-4 bg-gray-200"
 					style="width: 16%"
 				></div>
@@ -18,26 +18,26 @@
 			<!-- step details -->
 			<div class="flex flex-col items-center md:items-start md:flex-row px-8 py-10 border-2 border-gray-900 bg-white">
 				<div>
-					<img :src="steps[currentProfileCreationStep].iconUrl" class="w-14 mb-6 md:mb-0" />
+					<img :src="steps[currentStep].iconUrl" class="w-14 mb-6 md:mb-0" />
 				</div>
 				<div class="md:pl-8">
 					<!-- step title -->
-					<div class="mb-3 font-bold text-black text-5-1/2 text-center md:text-left">{{ steps[currentProfileCreationStep].title }}</div>
+					<div class="mb-3 font-bold text-black text-5-1/2 text-center md:text-left">{{ steps[currentStep].title }}</div>
 					<!-- step description -->
 					<div
 						class="font-normal text-black text-base text-center md:text-left"
-					>{{ steps[currentProfileCreationStep].description }}</div>
+					>{{ steps[currentStep].description }}</div>
 				</div>
 			</div>
 			<div class="flex justify-between lg:justify-end items-center mt-10">
 				<button
-					@click="prevStep"
-					v-if="currentProfileCreationStep !== 1"
+					@click="prev"
+					v-if="currentStep !== 1"
 					class="secondary-btn mr-16"
 				>
 					back
 				</button>
-				<button @click="next" class="primary-btn">{{ steps[currentProfileCreationStep].nextButtonText }}</button>
+				<button @click="next" class="primary-btn">{{ steps[currentStep].nextButtonText }}</button>
 			</div>
 		</div>
 		<div class="md:hidden absolute top-0 bottom-0 left-1/2 w-screen -transform-x-50 bg-gray-100"></div>
@@ -45,7 +45,7 @@
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex';
+import { mapState } from 'vuex';
 import steps from './steps';
 // eslint-disable-next-line no-unused-vars
 
@@ -56,19 +56,28 @@ export default {
 		...mapState('employer', ['currentProfileCreationStep'])
 	},
 
+	watch: {
+		currentProfileCreationStep(newVal) {
+			this.currentStep = newVal;
+		}
+	},
+
 	data() {
 		return {
 			totalSteps: [0,1,2,3,4,5],
+			currentStep: 0,
 			steps
 		};
 	},
 
-	mounted() {
-		// listen to some events so we can move automatically to next step
+	created() {
+		this.currentStep = JSON.parse(JSON.stringify(this.currentProfileCreationStep));
 	},
 
 	methods: {
-		...mapMutations('employer', ['prevStep']),
+		prev() {
+			this.currentStep -= 1;
+		},
 
 		next() {
 			this.$bus.$emit(
