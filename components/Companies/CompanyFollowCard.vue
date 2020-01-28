e9cf76c239f68c00c135b2cd6c3aac42f2cf19b2
Hamza <hamzabitup@gmail.com>
2020-01-28 11:31:36 +0100

    Add: companyFollowCard component

---
+++ components/Companies/CompanyFollowCard.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="flex flex-col w-32 h-40 border border-gray-100 bg-white">
+		<div class="flex justify-center items-center flex-grow p-4">
+			<img :src="logoUrl" alt />
+		</div>
+		<div
+			class="flex items-center justify-center relative h-12-1/2 bg-gray-800"
+		>
+			<base-follow-button class="text-center" />
+		</div>
+	</div>
+</template>
+
+<script>
+import BaseFollowButton from '~/components/BaseComponents/BaseFollowButton';
+
+export default {
+    name: 'CompanyFollowCard',
+    
+    props: {
+        logoUrl: {
+            type: String,
+            required: false,
+            default: ''
+        }
+    },
+
+	components: {
+		BaseFollowButton
+	}
+};
+</script>
