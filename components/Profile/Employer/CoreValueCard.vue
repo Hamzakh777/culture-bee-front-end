87e4fe5dddfa7f59a1dfd1eb4ca384e11e0b0be1
Hamza <hamzabitup@gmail.com>
2020-01-30 17:40:10 +0100

    Fix: text overflow

--- components/Profile/Employer/CoreValueCard.vue
+++ components/Profile/Employer/CoreValueCard.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="card w-full lg:w-31-% mb-8 px-8 py-12 bg-white">
         <!-- title -->
-        <div class="mb-4 lg:mb-6 font-D-Din text-8 lg:text-5xl uppercase font-bold text-gray-800">
+        <div class="mb-4 lg:mb-6 font-D-Din text-8 lg:text-5xl uppercase font-bold text-gray-800" style="overflow-wrap: break-word;">
             {{ value.title }}
         </div>
         <!-- sub title -->
-        <div class="mb-6 text-5-1/2 font-bold text-gray-800">
+        <div class="mb-6 text-5-1/2 font-bold text-gray-800" style="overflow-wrap: break-word;">
             {{ value.subtitle }}
         </div>
         <!-- description -->
-        <div class="text-xs md:text-base font-normal text-gray-800">
+        <div class="text-xs md:text-base font-normal text-gray-800" style="overflow-wrap: break-word;">
             {{ value.description }}
         </div>
     </div>
