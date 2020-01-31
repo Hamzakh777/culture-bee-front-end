ad64d5d8f32438544a0c0a7c614c95f384ea97f4
Hamza <hamzabitup@gmail.com>
2020-01-31 17:47:33 +0100

    Add: actions dropdown to job card

--- components/Jobs/JobCard.vue
+++ components/Jobs/JobCard.vue
@@ -1,52 +1,59 @@
 <template>
-	<div 
-		class="flex flex-col md:flex-row justify-between items-stretch mb-4 lg:mb-8 px-0 md:px-10 pt-10 md:py-12 border border-gray-300 cursor-pointer"
+	<div
 		@click="viewJob"
+		class="flex flex-col md:flex-row justify-between items-stretch mb-4 lg:mb-8 px-0 md:px-10 pt-10 md:py-12 border border-gray-300 cursor-pointer"
 	>
 		<!-- company logo and follow button -->
-		<div 
+		<div
 			class="hidden lg:flex flex-col w-32 h-40 border border-gray-100 bg-white"
 		>
 			<div class="flex justify-center items-center flex-grow p-4">
 				<img :src="job.owner.profile_img_url" />
 			</div>
-			<div class="flex items-center justify-center relative h-12-1/2 bg-gray-800">
+			<div
+				class="flex items-center justify-center relative h-12-1/2 bg-gray-800"
+			>
 				<base-follow-button class="text-center" />
 			</div>
 		</div>
 		<!-- job details -->
-		<div class="flex flex-col justify-between mb-5 md:mb-0 px-6 md:px-10 pt-0 md:pt-4 flex-grow">
+		<div
+			class="flex flex-col justify-between mb-5 md:mb-0 px-6 md:px-10 pt-0 md:pt-4 flex-grow"
+		>
 			<div class="mb-4">
 				<div class="flex items-start justify-start mb-2">
 					<!-- seniority -->
 					<div class="sub-title mr-4">{{ job.seniority }}</div>
 					<!-- logo -->
-					<img 
+					<img
+						:src="job.owner.profile_img_url"
 						class="block lg:hidden h-5"
 						style="max-width: 140px"
-						:src="job.owner.profile_img_url" 
-					>
+					/>
 				</div>
 				<!-- job title -->
-				<div class="text-5-1/2 lg:text-8 font-bold text-gray-800">{{ job.jobTitle }}</div>
+				<div class="text-5-1/2 lg:text-8 font-bold text-gray-800">
+					{{ job.jobTitle }}
+				</div>
 			</div>
 			<!-- location -->
 			<div class="small-text">{{ job.location }}</div>
-            <!-- job tags  -->
-            <div 
-                v-if="isJobSeekerView" 
-                class="flex"
-            >
+			<!-- job tags  -->
+			<div v-if="isJobSeekerView" class="flex">
 				<div
 					v-for="(tag, index) in job.tags"
 					:key="index"
 					:class="{ 'ml-5': index !== 0 }"
 					class="small-text"
-				>{{ tag }}</div>
+				>
+					{{ tag }}
+				</div>
 			</div>
 		</div>
 		<!-- job tags and date -->
-		<div class="flex flex-col justify-between px-6 md:px-0 mb-8 lg:mb-0 text-right">
+		<div
+			class="flex flex-col justify-between px-6 md:px-0 mb-8 lg:mb-0 text-right"
+		>
 			<base-like-icon v-if="isJobSeekerView" class="relative h-19" />
 			<!-- created at -->
 			<div class="hidden md:block small-text">{{ job.createdAt }}</div>
@@ -57,22 +64,66 @@
 					:key="index"
 					:class="{ 'ml-5': index !== 0 }"
 					class="small-text"
-				>{{ tag }}</div>
+				>
+					{{ tag }}
+				</div>
 			</div>
 		</div>
+		<div class="hidden lg:flex flex-col items-end justify-between pl-16">
+			<div class="relative">
+				<button
+					class="flex justify-center items-center h-12 w-12 bg-gray-800 focus:outline-none"
+					@click.stop="toggleActionsDropdown"
+				>
+					<div 
+						class="h-1-1/2 w-1-1/2 mx-1/2 rounded-full bg-yellow"
+						v-for="i in 3" 
+						:key="i"
+					>
+					</div>
+				</button>
+				<!-- actions dropdown  -->
+				<div 
+					v-if="isActionsDropdownActive"
+					class="absolute top-0 right-0 mt-14 bg-white shadow-md"
+				>
+					<button
+						class="action-btn border-gray-300 border-b-2"
+					>
+						Delete
+					</button>
+					<button
+						class="action-btn"
+					>
+						Expire
+					</button>
+				</div>
+			</div>
+			<button 
+				class="primary-btn"
+				@click.stop="$bus.$emit('open-edit-job-modal')"
+			>
+				Edit job
+			</button>
+		</div>
 		<!-- follow card - shows up on mobile only -->
-		<div class="flex lg:hidden items-center justify-center relative h-12-1/2 bg-gray-800">
+		<div
+			class="flex lg:hidden items-center justify-center relative h-12-1/2 bg-gray-800"
+		>
 			<base-follow-button class="text-center" />
 		</div>
 	</div>
 </template>
 
 <script>
+import { mixin as clickaway } from 'vue-clickaway';
 import BaseFollowButton from '~/components/BaseComponents/BaseFollowButton';
 import BaseLikeIcon from '~/components/BaseComponents/BaseLikeIcon';
 
 export default {
 	name: 'JobCarb',
+
+	mixins: [clickaway],
 
 	components: {
 		BaseFollowButton,
@@ -95,11 +146,16 @@ export default {
 
 	data() {
 		return {
-			tags: ['FASHION', 'Ethical', 'hacking']
+			tags: ['FASHION', 'Ethical', 'hacking'],
+			isActionsDropdownActive: false
 		};
 	},
 
 	methods: {
+		toggleActionsDropdown() {
+			this.isActionsDropdownActive = !this.isActionsDropdownActive;
+		},
+
 		/**
 		 * Toggle following the comany
 		 */
@@ -111,3 +167,8 @@ export default {
 	}
 };
 </script>
+<style scoped>
+.action-btn {
+	@apply px-6 py-4 text-sm font-bold text-black;
+}	
+</style>
