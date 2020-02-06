9348f4b5c76687a6e99e4cf8e62394c583269c11
Hamza <hamzabitup@gmail.com>
2020-02-06 17:20:55 +0100

    Add: employer vision section placeholder content

--- components/Profile/Employer/Sections/EmployerCompanyVisionSection.vue
+++ components/Profile/Employer/Sections/EmployerCompanyVisionSection.vue
@@ -4,7 +4,9 @@
 			<div class="base-title mb-8">Company vision</div>
 			<div class="max-w-4xl md:px-8">
 				<div class="text-5-1/2 font-bold text-gray-800">
-					<base-splited-paragraphs :text="description" />
+					<base-splited-paragraphs 
+						:text="descriptionToShow" 
+					/>
 				</div>
 			</div>
 			<!-- edit button -->
@@ -24,6 +26,7 @@
 import { mapState, mapActions } from 'vuex';
 import BaseSplitedParagraphs from '~/components/BaseComponents/BaseSplitedParagraphs';
 import BaseEditPen from '~/components/BaseComponents/BaseEditPen';
+import dummyVision from '~/mocks/Employer/vision';
 
 export default {
 	name: 'EmployerCompanyVisionSection',
@@ -42,7 +45,17 @@ export default {
 	},
 
 	computed: {
-		...mapState('employer/vision', ['description'])
+		...mapState('employer/vision', ['description']),
+		/**
+		 * Since we want to show dummy content when the user has not inputed his
+		 */
+		descriptionToShow() {
+			if(this.isEditPage && (this.description === '' || this.description === undefined)) {
+				return dummyVision;
+			} else {
+				return this.description;
+			}
+		}
 	},
 
 	mounted() {
@@ -50,7 +63,6 @@ export default {
 			this.fetchVision(this.$route.params.id);
 		} catch (error) {
 			alert('An error happend trying to load company vision');
-			console.error(error);
 		}
 	},
 
---
+++ mocks/Employer/vision.js
@@ -0,0 +1,3 @@
+const vision = `Here's where you add your vision, this is your opportunity to insipre! If you don't have a defined vision statement, think about why you do what you do, and the different you hope to make \n Here's where you expand on your vision with your mission and what you're tyring to achieve. Avoid being too salesy. Remember your audience here is job seekers.`;
+
+export default vision;
