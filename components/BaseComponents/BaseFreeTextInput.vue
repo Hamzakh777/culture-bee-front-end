a33d14e193faa6cd2d2c1642f852f7b3b4708f83
Hamza <hamzabitup@gmail.com>
2020-02-11 14:52:51 +0100

    Add: error handling for free text input component

--- components/BaseComponents/BaseFreeTextInput.vue
+++ components/BaseComponents/BaseFreeTextInput.vue
@@ -1,37 +1,59 @@
 <template>
-    <div
-        class="relative flex items-center h-14 pl-4 pr-10 border-2 border-gray-700 bg-white overflow-hidden"
-    >
-        <input 
-            class="text-sm text-gray-900 font-semibold focus:outline-none"
-            v-model="text"
-            type="text"
-            @keypress.enter="add"
+	<div>
+        <div
+            class="relative flex items-center h-14 pl-4 pr-10 border-2 border-gray-700 bg-white overflow-hidden"
+            :class="{'mb-4': isError}"
         >
-        <img 
-            class="h-4 absolute right-0 top-1/2 -transform-y-50 transition-all transition-100 transition-ease-out cursor-pointer"
-            :class="{'opacity-100 mr-4': text.length !== 0, ' opacity-0 -mr-10': text.length === 0 }"
-            src="/icons/checkmark.svg" 
-            @click="add"
-        >
+            <input
+                class="text-sm text-gray-900 font-semibold focus:outline-none"
+                v-model.trim="text"
+                type="text"
+                @keypress.enter="add"
+            />
+            <img
+                class="h-4 absolute right-0 top-1/2 -transform-y-50 transition-all transition-100 transition-ease-out cursor-pointer"
+                :class="{
+                    'opacity-100 mr-4': text.length !== 0,
+                    ' opacity-0 -mr-10': text.length === 0
+                }"
+                src="/icons/checkmark.svg"
+                @click="add"
+            />
+        </div>
+        <base-input-error-message 
+            v-if="isError" 
+            :error-type="'required'" 
+        />
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'BaseFreeTextInput',
+import BaseInputErrorMessage from '~/components/BaseComponents/BaseInputErrorMessage';
 
-        data() {
-            return {
-                text: ''
-            }
-        },
+export default {
+	name: 'BaseFreeTextInput',
 
-        methods: {
-            add() {
-                this.$emit('add', this.text);
-                this.text = '';
-            }
-        },
-    }
+	components: {
+		BaseInputErrorMessage
+	},
+
+	data() {
+		return {
+			text: '',
+			isError: false
+		};
+	},
+
+	methods: {
+		add() {
+			if (this.text.length === 0) {
+				this.isError = true;
+			} else {
+				this.$emit('add', this.text);
+				this.isError = false;
+				this.text = '';
+			}
+		}
+	}
+};
 </script>
