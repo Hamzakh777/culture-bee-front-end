a28e8dfef6d5aa9f5bed8f8d79d373dc529267e5
Hamza <hamzabitup@gmail.com>
2020-02-19 00:26:22 +0100

    Fix: change logo

--- components/BaseComponents/BaseLogo.vue
+++ components/BaseComponents/BaseLogo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <img 
-            :src="isLight ? '/logo-big-light.svg' : '/logo-big.png'" 
+            :src="isLight ? '/logo-big-light.png' : '/logo-big.png'" 
             class="hidden md:inline-block h-11-1/4"
             alt="culturebee logo"
         >
