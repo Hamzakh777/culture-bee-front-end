79433b9aa30a95e65062c4f8d5059b73c6fcd1b4
Hamza <hamzabitup@gmail.com>
2020-02-23 16:29:52 +0100

    Fix: navbar search button should be a link

--- components/NavBar/SearchButton.vue
+++ components/NavBar/SearchButton.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="relative h-12-1/2 w-12-1/2 bg-gray-800 text-white">
+    <nuxt-link 
+        class="relative h-12-1/2 w-12-1/2 bg-gray-800 text-white"
+        to="/search"
+    >
         <BaseAppIcon
             :name="'search'"
             :classList="['absolute', 'top-1/2', 'left-1/2', 'h-6', 'transform-center', 'fill-current', 'stroke-current']"
         />
-    </div>
+    </nuxt-link>
 </template>
 
 <script>
