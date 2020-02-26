517721509a65cccbdc148ada45151dcbe7f91b20
Hamza <hamzabitup@gmail.com>
2020-02-26 23:01:27 +0100

    Change: mock vision text

--- mocks/Employer/vision.js
+++ mocks/Employer/vision.js
@@ -1,3 +1,4 @@
-const vision = `Here's where you add your vision, this is your opportunity to insipre! If you don't have a defined vision statement, think about why you do what you do, and the different you hope to make \n Here's where you expand on your vision with your mission and what you're tyring to achieve. Avoid being too salesy. Remember your audience here is job seekers.`;
+const vision = `Here's where you add your vision, this is your opportunity to inspire! If you don't have a defined vision statement, think about why you do what you do, and the difference you hope to make \n
+Here's where you expand on your vision with your mission and what you're trying to achieve. Avoid being too salesy. Remember your audience here is job seekers.`;
 
 export default vision;
